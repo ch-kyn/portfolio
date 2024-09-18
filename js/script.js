c0734621bf07363de6d26b3a9087d304702e96f0
ch-kyn <chriskng@stud.ntnu>
2024-09-18 04:11:45 +0200

    before I regret not saving

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 const dropdownBtn = document.querySelector(".dropdown__button");
 const dropdownContent = document.querySelector(".dropdown__content");
+const menu = document.querySelector("ul.menu");
+
+/* menu.addEventListener ("click", () => {
+  event.stopPropagation();
+  if (dropdownContent.style.display === "none") {
+    dropdownContent.style.display = "block";
+  } else {
+    dropdownContent.style.display = "none";
+  }
+})  */
 
 dropdownBtn.addEventListener ("click", () => {
-    if (dropdownContent.style.display === "none") {
-        dropdownContent.style.display = "block";
-      } else {
-        dropdownContent.style.display = "none";
-      }
+  if (dropdownContent.style.display === "none") {
+      dropdownContent.style.display = "block";
+    } else {
+      dropdownContent.style.display = "none";
+    }
 })
+
+/* document.addEventListener('click', (e) => {
+  if (!e.target.closest('.dropdownContent') || !menu.contains(e.target)) 
+    dropdownContent.style.display = "none";
+ }) */
+
+// document.addEventListener("click", (e) => {
+//  if (!dropdownContent.contains(e.target) && e.target !== dropdownBtn) {
+//    dropdownContent.style.display = "none";
+// }
+// }) 
