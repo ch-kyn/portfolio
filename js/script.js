35b5b9e42f612baf0045011097e3bf2850945f0a
ch <chriskng@stud.ntnu.no>
2025-09-28 15:13:12 +0200

    Updates

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const dropdown = document.querySelector(".menu__dropdown");
+const menu = document.querySelector("ul.menu");
+const exploreBtn = document.querySelectorAll(".button--explore");
+const exitBtn = document.querySelectorAll(".button--exit");
+const homeCont = document.querySelector(".home-container");
+const overlays = document.querySelectorAll(".overlay");
+
+menu.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (dropdown.style.display === "none") {
+    dropdown.style.display = "block";
+  } else {
+    dropdown.style.display = "none";
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (!dropdown.contains(e.target) && dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+  }
+});
+
+menu.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter' || e.key === ' ') {
+    dropdown.style.display = "block";
+  }
+});
+
+exploreBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+
+    const overlayId = button.getAttribute("data-overlay");
+    const overlayToShow = document.getElementById(overlayId);
+    homeCont.style.display = "none";
+    overlayToShow.classList.add("overlay--active");
+  });
+});
+
+exitBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    overlays.forEach((overlay) => overlay.classList.remove("overlay--active"));
+    homeCont.style.display = "flex";
+  });
+});
+
